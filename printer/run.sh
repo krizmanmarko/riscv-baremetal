e2893f49d2886dad52fe64a28b334f9e0cdd61df
krizmanmarko <mk4462@student.uni-lj.si>
2025-04-25 12:43:10 +0200

    run.sh script for default configuration

--- printer/run.sh
+++ printer/run.sh
@@ -6,7 +6,7 @@ build_dir="./build"
 qemu-system-riscv64 \
 	-nographic \
 	-machine virt \
-	-kernel $build_dir/printer \
+	-kernel $build_dir/printer-riscv64-0x80100000 \
 	-cpu rv64 \
 	-smp 1 \
 	-m 6M \
